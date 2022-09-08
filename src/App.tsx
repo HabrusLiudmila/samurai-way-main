315325eb2494f98b83475ab0235ed81be1c5032e
HabrusLiudmila <odushko1@rambler.ru>
2022-09-08 21:48:29 +0300

    r

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Technologies from "./Technologiec";
+import Header from "./Header";
+
+function App() {
+    return (
+        <div>
+            <Header/>
+            <Technologies/>
+        </div>
+    );
+}
+export default App;
