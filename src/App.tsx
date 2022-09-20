c398b2a6113371cdc3c4447fab7751687820c552
HabrusLiudmila <odushko1@rambler.ru>
2022-09-20 14:44:10 +0300

    11 lesson

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Technologies from "./Technologiec";
 import Header from "./Header";
+import Footer from "./Footer";
 
 function App() {
     return (
         <div>
             <Header/>
             <Technologies/>
+            <Footer/>
         </div>
     );
 }
